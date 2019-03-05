b3d58e840a53cb0507d9dc3168f23b371a95ab11
Elena Simchenko <33508894+elenasimchenko@users.noreply.github.com>
2019-03-05 10:51:14 +0300

    Create CourseSiteTable.php

---
+++ lib/CourseSiteTable.php
@@ -0,0 +1,58 @@
+<?php
+namespace Learning;
+use Bitrix\Main\Entity;
+
+class CourseSiteTable extends Entity\DataManager
+{
+    /**
+     * Returns DB table name for entity.
+     *
+     * @return string
+     */
+    public static function getTableName()
+    {
+        return 'b_learn_course_site';
+    }
+
+    /**
+     * Returns entity map definition.
+     *
+     * @return array
+     */
+    public static function getMap()
+    {
+        return array(
+            new Entity\IntegerField('COURSE_ID', array(
+                'required' => true,
+                'primary' => true
+            )),
+            new Entity\ReferenceField(
+                'COURSE',
+                '\Learning\CourseTable',
+                array('=this.COURSE_ID' => 'ref.ID'),
+                array('join_type' => 'LEFT')
+            ),
+            new Entity\StringField('SITE_ID', array(
+                'primary' => true,
+                'validation' => array(__CLASS__, 'validateSiteId'),
+            )),
+            new Entity\ReferenceField(
+                'SITE',
+                '\Bitrix\Main\SiteTable',
+                array('=this.SITE_ID' => 'ref.LID'),
+                array('join_type' => 'LEFT')
+            ),
+        );
+    }
+    /**
+     * Returns validators for SITE_ID field.
+     *
+     * @return array
+     */
+    public static function validateSiteId()
+    {
+        return array(
+            new Main\Entity\Validator\Length(null, 2),
+        );
+    }
+}
